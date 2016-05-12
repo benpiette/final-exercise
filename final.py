221b70cdbe3827c802b4ee60af6957400de41410
Benjamin Piette <benjamin.piette@mail.utoronto.ca>
2016-05-12 19:16:23 -0400

    Final version!

--- final.py
+++ final.py
@@ -4,7 +4,7 @@ import pylab as pl
 import pandas as pd
 
 def main ():
-	data = pd.read_csv('gapminder_gdp_america.csv', index_col = 0, header = 0)
+	data = pd.read_csv('gapminder_gdp_americas.csv', index_col = 0, header = 0)
 	data_mean = data.mean(axis = 0)
 	x = ('1952', '1957', '1962', '1967', '1972', '1977', '1982', 
 '1987', '1992', '1997', '2002', '2007')
